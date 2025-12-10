3b2c7392a01c118dc5811210b7e040e6f12036ae
Алексей <50051634+Gloeend@users.noreply.github.com>
2025-12-10 23:10:44 +0700

    style(call-channel): grid of video tags (#12)
    style(channel-card): change fs to smaller

--- frontend/src/entities/channel/ui/channel-card.tsx
+++ frontend/src/entities/channel/ui/channel-card.tsx
@@ -7,8 +7,10 @@ export const ChannelCard = ({ id, className, onClick }: { id: string; className?
 			className={cn(className, 'size-full overflow-clip rounded-2xl bg-channel-gradient p-6 font-semibold transition-all')}
 		>
 			<div className='relative z-20 flex h-full flex-col gap-y-4'>
-				<h3 className='select-none text-2xl tracking-[-2%]'>UUID: {id}</h3>
+				<h3 className='select-none text-2xl tracking-[-2%]'>Без названия</h3>
 				<div className='mt-auto flex flex-col gap-y-2 tracking-default'>
+					<p className='text-sm font-normal text-tiny-900/40'>UUID</p>
+					<p className='text-xs font-light text-tiny-900'>{id}</p>
 					<p className='text-sm font-normal text-tiny-900/40'>Участников</p>
 					<p className='flex h-6 items-center gap-x-1'>
 						<span className='text-2xl font-semibold text-tiny-900'>3</span>
