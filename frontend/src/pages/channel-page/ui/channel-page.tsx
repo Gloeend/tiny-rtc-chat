2996fd6c5e164f98156ba490b7132e86b011049a
gloeend <alexey.larionov.fl@gmail.com>
2025-12-11 08:24:15 +0700

    style: replace "disconnect" from heading to media-panel.tsx, add copy to clipboard to heading

--- frontend/src/pages/channel-page/ui/channel-page.tsx
+++ frontend/src/pages/channel-page/ui/channel-page.tsx
@@ -1,24 +1,28 @@
 import { AppRoutes, RoutePath } from '@shared/config/routes-config'
+import { useCopyClipboardByAttribute } from '@shared/lib/hooks/use-copy-clipboard-by-attribute.ts'
 import { CallChannel } from '@widgets/call-channel'
-import { Navigate, useNavigate, useParams } from 'react-router'
+import { Copy } from 'lucide-react'
+import { Navigate, useParams } from 'react-router'
 
 export const ChannelPage = () => {
+	const onClickCopyClipboard = useCopyClipboardByAttribute<HTMLHeadingElement>('data-id')
 	const { id } = useParams()
-	const navigate = useNavigate()
 
 	if (!id) {
 		return <Navigate to={RoutePath[AppRoutes.ERROR].replace(':id', '404')} />
 	}
 
-	const leave = () => {
-		navigate(RoutePath[AppRoutes.MAIN])
-	}
-
 	return (
 		<main className='wrap grid min-h-dvh grid-rows-[fit-content(100%)_1fr] gap-y-8 px-4 pt-16'>
-			<section className='flex justify-between'>
-				<h1 className='text-2xl font-bold tracking-[-2%]'>Комната №{id}</h1>
-				<button onClick={leave}>Выйти</button>
+			<section>
+				<h1
+					onClick={onClickCopyClipboard}
+					data-id={id}
+					className='group/clipboard relative w-fit cursor-pointer text-2xl font-bold tracking-[-2%] active:opacity-50'
+				>
+					{id}
+					<Copy className='absolute -right-9 bottom-0 top-0 my-auto size-6 opacity-0 transition-opacity duration-75 group-hover/clipboard:opacity-100' />
+				</h1>
 			</section>
 			<CallChannel className='flex-shrink-1 h-full min-h-full' />
 		</main>
--- frontend/src/widgets/call-channel/ui/call-channel.tsx
+++ frontend/src/widgets/call-channel/ui/call-channel.tsx
@@ -1,18 +1,24 @@
 import { MediaPanel } from '@features/media-panel'
+import { AppRoutes, RoutePath } from '@shared/config/routes-config'
 import { cn } from '@shared/lib'
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 
 import { useCall } from '../lib/hooks/use-call'
 
 export const CallChannel = ({ className }: { className?: string }) => {
+	const navigate = useNavigate()
 	const { id = '' } = useParams()
 	const { ref, remoteRef } = useCall(id)
+
+	const onDisconnect = () => {
+		navigate(RoutePath[AppRoutes.MAIN])
+	}
 
 	return (
 		<section className={cn(className, 'grid grid-cols-3 gap-4 pb-24 max-768px-576px:grid-cols-2 max-576px:grid-cols-1')}>
 			<video className='bottom-4 right-4 h-[300px] w-full rounded-xl bg-black/50 object-contain' ref={ref} autoPlay muted />
 			<video className='block h-[300px] w-full rounded-xl bg-black/50 object-contain' ref={remoteRef} autoPlay />
-			<MediaPanel className='fixed bottom-4 left-0 right-0 mx-auto w-fit' />
+			<MediaPanel onDisconnect={onDisconnect} className='fixed bottom-4 left-0 right-0 mx-auto w-fit' />
 		</section>
 	)
 }
