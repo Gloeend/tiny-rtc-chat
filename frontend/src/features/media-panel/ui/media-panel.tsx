d4c2c752ee762667d0f64d5e4317b524fdd2f713
Алексей <50051634+Gloeend@users.noreply.github.com>
2025-12-11 08:25:18 +0700

    style: replace "disconnect" from heading to media-panel.tsx, add copy to clipboard to heading (#17)
    * fix: add channelId on batch socket event 'leave-room'
    * style: replace "disconnect" from heading to media-panel.tsx, add copy to clipboard to heading

--- frontend/src/features/media-panel/ui/media-panel.tsx
+++ frontend/src/features/media-panel/ui/media-panel.tsx
@@ -1,16 +1,26 @@
 import { cn } from '@shared/lib'
 import { LogOut, Mic, Video } from 'lucide-react'
 
-export const MediaPanel = ({ className }: { className?: string }) => {
+export const MediaPanel = ({ className, onDisconnect }: { className?: string; onDisconnect?: VoidFunction }) => {
 	return (
 		<div className={cn(className, 'flex gap-x-4 rounded-xl bg-accent px-6 py-2')}>
-			<button className='flex h-12 w-12 items-center justify-center rounded-xl bg-background/10 hover:bg-background'>
+			<button
+				type='button'
+				className='flex h-12 w-12 items-center justify-center rounded-xl bg-background/10 hover:bg-background'
+			>
 				<Mic />
 			</button>
-			<button className='flex h-12 w-12 items-center justify-center rounded-xl bg-background/10 hover:bg-background'>
+			<button
+				type='button'
+				className='flex h-12 w-12 items-center justify-center rounded-xl bg-background/10 hover:bg-background'
+			>
 				<Video />
 			</button>
-			<button className='flex h-12 w-12 items-center justify-center rounded-xl bg-destructive/50 hover:bg-destructive'>
+			<button
+				type='button'
+				onClick={onDisconnect}
+				className='flex h-12 w-12 items-center justify-center rounded-xl bg-destructive/50 hover:bg-destructive'
+			>
 				<LogOut />
 			</button>
 		</div>
